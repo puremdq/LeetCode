f45ad516d731848a3eacbab512e8f140c82c66f3
puremdq <puremdq@gmail.com>
2021-05-01 23:17:09 +0800

    Create [690]员工的重要性.java

---
+++ src/main/java/com/aojiaoo/leetcode/[690]员工的重要性.java
@@ -0,0 +1,107 @@
+package com.aojiaoo.leetcode;//给定一个保存员工信息的数据结构，它包含了员工 唯一的 id ，重要度 和 直系下属的 id 。
+//
+// 比如，员工 1 是员工 2 的领导，员工 2 是员工 3 的领导。他们相应的重要度为 15 , 10 , 5 。那么员工 1 的数据结构是 [1, 15,
+// [2]] ，员工 2的 数据结构是 [2, 10, [3]] ，员工 3 的数据结构是 [3, 5, []] 。注意虽然员工 3 也是员工 1 的一个下属，但
+//是由于 并不是直系 下属，因此没有体现在员工 1 的数据结构中。 
+//
+// 现在输入一个公司的所有员工信息，以及单个员工 id ，返回这个员工和他所有下属的重要度之和。 
+//
+// 
+//
+// 示例： 
+//
+// 
+//输入：[[1, 5, [2, 3]], [2, 3, []], [3, 3, []]], 1
+//输出：11
+//解释：
+//员工 1 自身的重要度是 5 ，他有两个直系下属 2 和 3 ，而且 2 和 3 的重要度均为 3 。因此员工 1 的总重要度是 5 + 3 + 3 = 1
+//1 。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 一个员工最多有一个 直系 领导，但是可以有多个 直系 下属 
+// 员工数量不超过 2000 。 
+// 
+// Related Topics 深度优先搜索 广度优先搜索 哈希表 
+// 👍 197 👎 0
+
+
+import com.aojiaoo.common.utils.JsonUtil;
+import lombok.extern.slf4j.Slf4j;
+import util.GenUtil;
+
+import java.util.*;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+// Definition for Employee.
+class Employee {
+    public int id;
+    public int importance;
+    public List<Integer> subordinates;
+
+    public Employee(int id, int importance, String subordinates) {
+        this.id = id;
+        this.importance = importance;
+        String[] split = subordinates.split(",");
+        this.subordinates = new ArrayList<>();
+
+        for (String s : split) {
+            if(s.length()>0){
+                this.subordinates.add(Integer.valueOf(s));
+            }
+        }
+    }
+};
+
+
+@Slf4j
+class Solution690 {
+    Map<Integer, Employee> map = new HashMap<>();
+
+    public int getImportance(List<Employee> employees, int id) {
+        int res = 0;
+        Employee needEmployee = null;
+        for (Employee employee : employees) {
+            map.put(employee.id, employee);
+            if (employee.id == id) {
+                needEmployee = employee;
+            }
+        }
+        if (needEmployee == null) {
+            return res;
+        }
+        return addImportance(needEmployee);
+    }
+
+    public int addImportance(Employee employee) {
+        int res=0;
+        if (employee == null) {
+            return res;
+        }
+        log.info("current id:{}",employee.id);
+        res = res + employee.importance;
+        List<Integer> subordinates = employee.subordinates;
+
+        if (subordinates != null && subordinates.size() > 0) {
+            for (Integer subordinate : subordinates) {
+                res = res + addImportance(map.get(subordinate));
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Solution690 solution = new Solution690();
+
+        List<Employee> list = new ArrayList<>();
+        list.add(new Employee(1, 5, "2,3"));
+        list.add(new Employee(2, 3, ""));
+        list.add(new Employee(3, 3, ""));
+        System.out.println(solution.getImportance(list, 1));
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
